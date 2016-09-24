f8235473c5b0d27a6f82e7646ccba930c27cecd1
MiguelPR <Miguel.Pari@jalasoft.com>
2016-09-24 07:26:46 -0400

    Initial code for Matrix class

--- js/Matrix.js
+++ js/Matrix.js
@@ -2,22 +2,32 @@
  * Created by Miguel.Pari on 9/23/2016.
  */
 
-var Matrix = function (size){
-
+var Matrix = function (size) {
+    this.matrix = [];
+    
+    for (var i = 0; i < size; i++) {
+        this.matrix.push([]);
+        this.matrix[i].push(new Array());
+        for (var j = 0; j < size; j++) {
+            this.matrix[i][j] = 0;
+        }
+    }
 };
 
-Matrix.prototype.FillPosition = function (x,y,value){
-
+Matrix.prototype.FillPosition = function (x, y, value) {
+    this.matrix[x][y] = value;
+    return;
 };
 
-Matrix.prototype.Display = function (){
-
+Matrix.prototype.GetPosition = function (x, y, value) {
+    return this.matrix[x][y];
 };
 
-Matrix.prototype.ReplacePosition = function (x,y,value){
-
+Matrix.prototype.Display = function () {
+    for (var i = 0; i < this.matrix[i].length; i++) {
+        for (var j = 0; j < this.matrix.length; j++) {
+            console.log(this.matrix[j][i]);
+        }
+    }
+    return;
 };
-
-
-
-
